fc2df82b73df16a46c6fe7ca2bc27021bb2ae2f6
kurobon-jp <kurobon.jp@gmail.com>
2026-04-14 11:31:27 +0900

    Update youtubei

--- server.ts
+++ server.ts
@@ -2,6 +2,8 @@ import { Innertube } from "youtubei.js";
 import * as esbuild from "esbuild";
 import { createKV } from "./kv.ts";
 
+const encoder = new TextEncoder();
+
 Deno.serve(async (req: Request) => {
     const url = new URL(req.url);
     const playerId = url.searchParams.get("player_id");
@@ -11,9 +13,9 @@ Deno.serve(async (req: Request) => {
 
     try {
         const kv = await createKV();
-        let code: string | undefined;
-        code = await kv.get(playerId);
-        if (!code) {
+        let buffer: ArrayBuffer | undefined;
+        buffer = await kv.get(playerId);
+        if (!buffer) {
             const innertube = await Innertube.create({ player_id: playerId })
             const playerData: any = innertube.session.player?.data
             if (!playerData) {
@@ -25,8 +27,9 @@ Deno.serve(async (req: Request) => {
                 minifyIdentifiers: true,
                 minifySyntax: true,
             });
-            code = result.code;
-            // await kv.set(playerId, code, 3600);
+
+            buffer = await compress(result.code);
+            await kv.set(playerId, buffer, 3600);
             esbuild.stop();
         }
 
@@ -34,8 +37,16 @@ Deno.serve(async (req: Request) => {
         {
             "cache-control": "public, max-age=86400"
         });
-        return new Response(code, { status: 200, headers });
+
+        return new Response(buffer, { status: 200, headers });
     } catch (error) {
         return new Response(`${error}`, { status: 500 });
     }
 });
+
+async function compress(str: string): Promise<ArrayBuffer> {
+  const cs = new CompressionStream("gzip");
+  const buf = encoder.encode(str);
+  const stream = new Response(buf).body!.pipeThrough(cs);
+  return new Response(stream).arrayBuffer();
+}
